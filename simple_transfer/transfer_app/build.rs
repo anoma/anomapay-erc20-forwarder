b8680739af0f4976b40c7b36cf32b35607f082d4
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-10-23 14:57:54 +0200

    refactor: move build script

--- simple_transfer/transfer_app/build.rs
+++ simple_transfer/transfer_app/build.rs
@@ -1,15 +1,17 @@
+//! A build script to compile the forwarder contract.
+
 use std::process::Command;
 
-// Example custom build script.
 fn main() {
-    // make sure forge is available
+    // Make sure forge is available.
     if Command::new("forge").arg("--version").output().is_err() {
         println!("cargo:warning=forge not found, skipping contract compilation");
         return;
     }
 
-    // run forge build --ast
+    // Run forge build --ast in the `../contracts` directory.
     let status = Command::new("forge")
+        .current_dir("../../contracts")
         .args(["build", "--ast"])
         .status()
         .expect("failed to run forge command");
@@ -18,6 +20,6 @@ fn main() {
         panic!("forge build --ast command failed");
     }
 
-    // rebuild the contracts if any file changes in the contracts dir
-    println!("cargo:rerun-if-changed=./");
+    // Rebuild the contracts if any file has changed in the contracts dir.
+    println!("cargo:rerun-if-changed=../contracts/");
 }
