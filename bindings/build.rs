73c4428034a0fa4dd7619921e8b88b1c225b7c4a
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-11-21 15:59:53 +0100

    feat: add bindings

---
+++ bindings/build.rs
@@ -0,0 +1,23 @@
+use std::process::Command;
+
+// Example custom build script.
+fn main() {
+    // Ensure forge is available.
+    if Command::new("forge").arg("--version").output().is_err() {
+        panic!("Forge not found. Visit https://getfoundry.sh/ to install it.");
+    }
+
+    // Run `forge build --ast` in the `../contracts` directory.
+    let status = Command::new("forge")
+        .current_dir("../contracts")
+        .args(["build", "--ast"])
+        .status()
+        .expect("failed to run forge command");
+
+    if !status.success() {
+        panic!("forge build --ast command failed");
+    }
+
+    // Rebuild the contracts if any file changes in the contracts dir
+    println!("cargo:rerun-if-changed=../contracts/");
+}
---
+++ bindings/src/addresses.rs
@@ -0,0 +1,13 @@
+use alloy::primitives::Address;
+use alloy_chains::NamedChain;
+use std::collections::HashMap;
+
+/// Returns a map of protocol adapter deployments for all supported chains.
+pub fn forwarder_deployments_map() -> HashMap<NamedChain, Address> {
+    HashMap::from([])
+}
+
+/// Returns the address of the protocol adapter deployed on the provided chain, if any.
+pub fn forwarder_address(chain: NamedChain) -> Option<Address> {
+    forwarder_deployments_map().get(&chain).cloned()
+}
