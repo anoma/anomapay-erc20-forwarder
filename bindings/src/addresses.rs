5222b7fa6826b743da8964e76aacce51f92ca721
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-11-25 13:02:57 +0100

    doc: fix comment

--- bindings/src/addresses.rs
+++ bindings/src/addresses.rs
@@ -2,12 +2,12 @@ use alloy::primitives::Address;
 use alloy_chains::NamedChain;
 use std::collections::HashMap;
 
-/// Returns a map of protocol adapter deployments for all supported chains.
+/// Returns a map of ERC20 forwarder contract deployments for all supported chains.
 pub fn forwarder_deployments_map() -> HashMap<NamedChain, Address> {
     HashMap::from([])
 }
 
-/// Returns the address of the protocol adapter deployed on the provided chain, if any.
+/// Returns the address of the ERC20 forwarder contract  deployed on the provided chain, if any.
 pub fn forwarder_address(chain: &NamedChain) -> Option<Address> {
     forwarder_deployments_map().get(chain).cloned()
 }
