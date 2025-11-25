cdfff3c921e7009d5a3e694fac259055e5ba23ad
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-11-25 18:25:58 +0100

    feat: deployed forwarder on sepolia

--- bindings/src/addresses.rs
+++ bindings/src/addresses.rs
@@ -1,10 +1,14 @@
-use alloy::primitives::Address;
+use alloy::primitives::{address, Address};
 use alloy_chains::NamedChain;
 use std::collections::HashMap;
 
 /// Returns a map of ERC20 forwarder contract deployments for all supported chains.
 pub fn erc20_forwarder_deployments_map() -> HashMap<NamedChain, Address> {
-    HashMap::from([])
+    use NamedChain::*;
+    HashMap::from([(
+        Sepolia,
+        address!("0xfAeFAAa7E71A97A575bb3fc79A8FB4FD49fd47bd"),
+    )])
 }
 
 /// Returns the address of the ERC20 forwarder contract  deployed on the provided chain, if any.
