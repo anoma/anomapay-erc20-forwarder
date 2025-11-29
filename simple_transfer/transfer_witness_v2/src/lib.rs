813ea42007f6a58bb4afef963cddd1c78d3a5b3b
agureev <artguril@proton.me>
2025-11-29 20:18:30 +0400

    Add Encryption Key To Value in V2 Transfer Circuit

--- simple_transfer/transfer_witness_v2/src/lib.rs
+++ simple_transfer/transfer_witness_v2/src/lib.rs
@@ -13,12 +13,14 @@ use arm::{
     utils::bytes_to_words,
     Digest,
 };
-use arm_gadgets::{encryption::Ciphertext, evm::ForwarderCalldata};
+use arm_gadgets::{
+    authorization::AuthorizationSignature, encryption::Ciphertext, evm::ForwarderCalldata,
+};
 use serde::{Deserialize, Serialize};
 use transfer_witness::{
-    calculate_label_ref, calculate_value_ref_from_auth, calculate_value_ref_from_user_addr,
+    calculate_label_ref, calculate_persistent_value_ref, calculate_value_ref_from_user_addr,
     call_type::{encode_unwrap_forwarder_input, encode_wrap_forwarder_input, PermitTransferFrom},
-    AuthorizationInfo, DeletionCriterion, EncryptionInfo, LabelInfo, PermitInfo, ResourceWithLabel,
+    DeletionCriterion, EncryptionInfo, LabelInfo, PermitInfo, ResourceWithLabel, ValueInfo,
 };
 
 pub const AUTH_SIGNATURE_DOMAIN_V2: &[u8] = b"TokenTransferAuthorizationV2";
@@ -35,14 +37,16 @@ pub struct TokenTransferWitnessV2 {
     pub action_tree_root: Digest,
     /// Nullifier key for the resource.
     pub nf_key: Option<NullifierKey>,
-    /// See AuthorizationInfo struct.
-    pub auth_info: Option<AuthorizationInfo>,
+    /// A consumed persistent resource requires an authorization signature
+    pub auth_sig: Option<AuthorizationSignature>,
     /// See EncryptionInfo struct.
     pub encryption_info: Option<EncryptionInfo>,
     /// See ForwarderInfoV2 struct.
     pub forwarder_info_v2: Option<ForwarderInfoV2>,
     /// See LabelInfo struct.
     pub label_info: Option<LabelInfo>,
+    /// See ValueInfo Struct
+    pub value_info: Option<ValueInfo>,
 }
 
 #[derive(Clone, Serialize, Deserialize)]
@@ -60,7 +64,8 @@ pub struct MigrateInfo {
     pub nf_key: NullifierKey,
     // Merkle path from cm-tree v1 to prove existence of the migrate_resource
     pub path: MerklePath,
-    pub auth_info: AuthorizationInfo,
+    pub auth_sig: AuthorizationSignature,
+    pub value_info: ValueInfo,
 }
 
 impl TokenTransferWitnessV2 {
@@ -75,6 +80,20 @@ impl TokenTransferWitnessV2 {
         } else {
             Ok(self.resource.commitment())
         }
+    }
+
+    // Check the value and return it unwrapped
+    pub fn value(&self) -> Result<&ValueInfo, ArmError> {
+        let value_info = self
+            .value_info
+            .as_ref()
+            .ok_or(ArmError::MissingField("Value info"))?;
+
+        if self.resource.value_ref != calculate_persistent_value_ref(&value_info) {
+            return Err(ArmError::InvalidResourceValueRef);
+        }
+
+        Ok(value_info)
     }
 
     // check on ephemeral resources and return external_payload
@@ -154,18 +173,21 @@ impl TokenTransferWitnessV2 {
                 assert!(!migrate_info.resource.is_ephemeral);
 
                 // check migrate_resource authorization
-                let auth_pk = &migrate_info.auth_info.auth_pk;
-                if migrate_info.resource.value_ref != calculate_value_ref_from_auth(auth_pk) {
+                if migrate_info.resource.value_ref
+                    != calculate_persistent_value_ref(&migrate_info.value_info)
+                {
                     return Err(ArmError::ProveFailed(
                         "Invalid migrate resource value_ref".to_string(),
                     ));
                 }
 
-                if auth_pk
+                if migrate_info
+                    .value_info
+                    .auth_pk
                     .verify(
                         AUTH_SIGNATURE_DOMAIN_V2,
                         action_root,
-                        &migrate_info.auth_info.auth_sig,
+                        &migrate_info.auth_sig,
                     )
                     .is_err()
                 {
@@ -210,20 +232,17 @@ impl TokenTransferWitnessV2 {
 
     // check persistent resource consumption
     pub fn persistent_resource_consumption(&self, action_root: &[u8]) -> Result<(), ArmError> {
-        let auth_info = self
-            .auth_info
+        let auth_sig = self
+            .auth_sig
             .as_ref()
-            .ok_or(ArmError::MissingField("Auth info"))?;
+            .ok_or(ArmError::MissingField("Auth signature"))?;
 
-        // check value_ref
-        if self.resource.value_ref != calculate_value_ref_from_auth(&auth_info.auth_pk) {
-            return Err(ArmError::InvalidResourceValueRef);
-        }
+        let value_info = self.value()?;
 
         // Verify the authorization signature
-        if auth_info
+        if value_info
             .auth_pk
-            .verify(AUTH_SIGNATURE_DOMAIN_V2, action_root, &auth_info.auth_sig)
+            .verify(AUTH_SIGNATURE_DOMAIN_V2, action_root, &auth_sig)
             .is_err()
         {
             return Err(ArmError::InvalidSignature);
@@ -251,6 +270,8 @@ impl TokenTransferWitnessV2 {
             ));
         }
 
+        let value_info = self.value()?;
+
         // Generate resource ciphertext
         let encryption_info = self
             .encryption_info
@@ -264,7 +285,7 @@ impl TokenTransferWitnessV2 {
         .map_err(|_| ArmError::InvalidResourceSerialization);
         let ciphertext = Ciphertext::encrypt_with_nonce(
             &payload_plaintext?,
-            &encryption_info.receiver_pk,
+            &value_info.encryption_pk,
             &encryption_info.sender_sk,
             encryption_info
                 .encryption_nonce
@@ -311,112 +332,7 @@ impl LogicCircuit for TokenTransferWitnessV2 {
         // Generate resource_payload and external_payload
         let (discovery_payload, resource_payload, external_payload) = if self.resource.is_ephemeral
         {
-            let forwarder_info_v2 = self
-                .forwarder_info_v2
-                .as_ref()
-                .ok_or(ArmError::MissingField("Forwarder info"))?;
-
-            let label_info = self
-                .label_info
-                .as_ref()
-                .ok_or(ArmError::MissingField("Label info"))?;
-
-            // Check resource label: label = sha2(forwarder_addr, erc20_addr)
-            let forwarder_addr = label_info.forwarder_addr.as_ref();
-            let erc20_addr = label_info.token_addr.as_ref();
-            let user_addr = forwarder_info_v2.user_addr.as_ref();
-            let label_ref = calculate_label_ref(forwarder_addr, erc20_addr);
-            assert_eq!(self.resource.label_ref, label_ref);
-
-            // Check resource value_ref: value_ref[0..20] = user_addr
-            let value_ref = calculate_value_ref_from_user_addr(user_addr);
-            assert_eq!(self.resource.value_ref, value_ref);
-
-            let input = match forwarder_info_v2.call_type {
-                CallTypeV2::Unwrap => {
-                    assert!(!self.is_consumed);
-                    encode_unwrap_forwarder_input(erc20_addr, user_addr, self.resource.quantity)
-                }
-                CallTypeV2::Wrap => {
-                    assert!(self.is_consumed);
-                    let permit_info = forwarder_info_v2
-                        .permit_info
-                        .as_ref()
-                        .ok_or(ArmError::MissingField("Permit info"))?;
-                    let permit = PermitTransferFrom::from_bytes(
-                        erc20_addr,
-                        self.resource.quantity,
-                        permit_info.permit_nonce.as_ref(),
-                        permit_info.permit_deadline.as_ref(),
-                    );
-                    encode_wrap_forwarder_input(
-                        user_addr,
-                        permit,
-                        root_bytes,
-                        permit_info.permit_sig.as_ref(),
-                    )
-                }
-                CallTypeV2::Migrate => {
-                    assert!(self.is_consumed);
-
-                    let migrate_info = forwarder_info_v2
-                        .migrate_info
-                        .as_ref()
-                        .ok_or(ArmError::MissingField("Migrate info"))?;
-
-                    // compute migrate resource commitment tree root
-                    let migrate_cm = migrate_info.resource.commitment();
-                    let migrate_root = migrate_info.path.root(&migrate_cm);
-
-                    // check migrate_resource is non-ephemeral
-                    assert!(!migrate_info.resource.is_ephemeral);
-
-                    // check migrate_resource authorization
-                    let auth_pk = &migrate_info.auth_info.auth_pk;
-                    assert_eq!(
-                        migrate_info.resource.value_ref,
-                        calculate_value_ref_from_auth(auth_pk)
-                    );
-                    assert!(auth_pk
-                        .verify(
-                            AUTH_SIGNATURE_DOMAIN_V2,
-                            root_bytes,
-                            &migrate_info.auth_info.auth_sig
-                        )
-                        .is_ok());
-
-                    // check migrate_resource quantity
-                    assert_eq!(migrate_info.resource.quantity, self.resource.quantity);
-
-                    // compute migrate resource nullifier
-                    let migrate_nf = migrate_info
-                        .resource
-                        .nullifier_from_commitment(&migrate_info.nf_key, &migrate_cm)?;
-
-                    encode_migrate_forwarder_input(
-                        erc20_addr,
-                        self.resource.quantity,
-                        migrate_nf.as_bytes(),
-                        migrate_root.as_bytes(),
-                        migrate_info.resource.logic_ref.as_bytes(),
-                        migrate_info.resource.label_ref.as_bytes(),
-                    )
-                }
-                _ => {
-                    return Err(ArmError::MissingField(
-                        "Invalid call type for ephemeral resource",
-                    ));
-                }
-            };
-
-            let forwarder_call_data = ForwarderCalldata::from_bytes(forwarder_addr, input, vec![]);
-            let external_payload = {
-                let call_data_expirable_blob = ExpirableBlob {
-                    blob: bytes_to_words(&forwarder_call_data.encode()),
-                    deletion_criterion: DeletionCriterion::Never as u32,
-                };
-                vec![call_data_expirable_blob]
-            };
+            let external_payload = self.ephemeral_resource_check(root_bytes)?;
 
             // Empty discovery_payload and resource_payload
             (vec![], vec![], external_payload)
@@ -458,20 +374,22 @@ impl TokenTransferWitnessV2 {
         is_consumed: bool,
         action_tree_root: Digest,
         nf_key: Option<NullifierKey>,
-        auth_info: Option<AuthorizationInfo>,
+        auth_sig: Option<AuthorizationSignature>,
         encryption_info: Option<EncryptionInfo>,
         forwarder_info_v2: Option<ForwarderInfoV2>,
         label_info: Option<LabelInfo>,
+        value_info: Option<ValueInfo>,
     ) -> Self {
         Self {
             is_consumed,
             resource,
             action_tree_root,
             nf_key,
-            auth_info,
+            auth_sig,
             encryption_info,
             forwarder_info_v2,
             label_info,
+            value_info,
         }
     }
 }
