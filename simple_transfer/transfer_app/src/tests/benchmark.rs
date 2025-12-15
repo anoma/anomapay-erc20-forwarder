80591eb27d23c44fec4b6ce36d6ada197d98b48e
Christophe De Troyer <christophe.detroyer@gmail.com>
2025-12-15 15:43:04 +0100

    update based on remarks

--- simple_transfer/transfer_app/src/tests/benchmark.rs
+++ simple_transfer/transfer_app/src/tests/benchmark.rs
@@ -7,7 +7,10 @@ use crate::{AnomaPayConfig, load_config};
 use futures::future::join_all;
 
 /// Create `count` mint transactions concurrently.
-/// A mint transaction generates 2 logic proofs and 1 compliance proof.
+///
+/// A mint transaction generates 2 logic proofs and 1 compliance proof. If proof
+/// aggregation is enabled, there is a fourth proof that is generated after the
+/// first 3 proofs have been generated concurrently.
 async fn mint(config: &AnomaPayConfig, alice: Keychain, count: i32) {
     let mut i = 0;
     let mut transaction_futures = vec![];
@@ -20,7 +23,7 @@ async fn mint(config: &AnomaPayConfig, alice: Keychain, count: i32) {
     let _x = join_all(transaction_futures).await;
 }
 
-/// Generate a test for various concurrency.
+/// Test that creates `CONCURRENCY` mint transactions concurrently.
 /// This test is not supposed to be run each time, so it is ignored.
 /// Control concurrency with env var `CONCURRENCY` to determine how many transactions you want to generate at
 /// once.
@@ -30,9 +33,9 @@ async fn mint(config: &AnomaPayConfig, alice: Keychain, count: i32) {
 #[tokio::test]
 async fn benchmark_mint() {
     let num: i32 = std::env::var("CONCURRENCY")
-        .expect("CONCURRENCY env var must be set to an integer between 1 and infinity")
+        .expect("CONCURRENCY env var must be set to an integer between 1 and i32 max")
         .parse()
-        .expect("CONCURRENCY env var must be set to an integer between 1 and infinity");
+        .expect("CONCURRENCY env var must be set to an integer between 1 and i32 max");
 
     let config = load_config().await.expect("failed to load config in test");
     let alice = user_with_private_key(&config);
