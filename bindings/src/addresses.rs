4084e44f7a9a1bee2aed348394f5a419475d21ae
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-12-16 22:43:10 +0100

    feat: deployed to base-sepolia

--- bindings/src/addresses.rs
+++ bindings/src/addresses.rs
@@ -5,10 +5,16 @@ use std::collections::HashMap;
 /// Returns a map of ERC20 forwarder contract deployments for all supported chains.
 pub fn erc20_forwarder_deployments_map() -> HashMap<NamedChain, Address> {
     use NamedChain::*;
-    HashMap::from([(
-        Sepolia,
-        address!("0x9bbC75f66f5344864FfABCF70625B99E42e5F108"),
-    )])
+    HashMap::from([
+        (
+            Sepolia,
+            address!("0x9bbC75f66f5344864FfABCF70625B99E42e5F108"),
+        ),
+        (
+            BaseSepolia,
+            address!("0xfeE5b48919100c192E017992810c9539ebD00249"),
+        ),
+    ])
 }
 
 /// Returns the address of the ERC20 forwarder contract  deployed on the provided chain, if any.
