124fedc878a89c45ef1be80a619c915d3cb136b7
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-01-27 16:52:53 +0100

    chore: bump anoma-pa-evm to 1.1.0 and update version numbers

--- bindings/src/addresses.rs
+++ bindings/src/addresses.rs
@@ -7,27 +7,27 @@ pub fn erc20_forwarder_deployments_map() -> HashMap<NamedChain, Address> {
     HashMap::from([
         (
             NamedChain::Sepolia,
-            address!("0xa04942494174eD85A11416E716262eC0AE0a065d"),
+            address!("0x0000000000000000000000000000000000000000"),
         ),
         (
             NamedChain::Mainnet,
-            address!("0x0D38C332135f9f0de4dcc4a6F9c918b72e2A1Df3"),
+            address!("0x0000000000000000000000000000000000000000"),
         ),
         (
             NamedChain::BaseSepolia,
-            address!("0xA73Ce304460F17C3530b58BA95bCD3B89Bd38D69"),
+            address!("0x0000000000000000000000000000000000000000"),
         ),
         (
             NamedChain::Base,
-            address!("0xA73Ce304460F17C3530b58BA95bCD3B89Bd38D69"),
+            address!("0x0000000000000000000000000000000000000000"),
         ),
         (
             NamedChain::Optimism,
-            address!("0xA73Ce304460F17C3530b58BA95bCD3B89Bd38D69"),
+            address!("0x0000000000000000000000000000000000000000"),
         ),
         (
             NamedChain::Arbitrum,
-            address!("0xA73Ce304460F17C3530b58BA95bCD3B89Bd38D69"),
+            address!("0x0000000000000000000000000000000000000000"),
         ),
     ])
 }
--- contracts/test/examples/Transaction.e.sol
+++ contracts/test/examples/Transaction.e.sol
@@ -1,14 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import {Transaction} from "anoma-pa-evm-1.0.0/src/Types.sol";
-import {Parsing} from "anoma-pa-evm-1.0.0/test/libs/parsing.sol";
+import {Transaction} from "anoma-pa-evm-1.1.0/src/Types.sol";
+import {Parsing} from "anoma-pa-evm-1.1.0/test/libs/parsing.sol";
 import {Vm} from "forge-std-1.14.0/src/Test.sol";
 
 library TransactionExample {
     using Parsing for Vm;
 
     function exampleTransaction(Vm vm) public view returns (Transaction memory txn) {
-        txn = vm.parseTransaction("dependencies/anoma-pa-evm-1.0.0/test/examples/transactions/test_tx_reg_01_01.bin");
+        txn = vm.parseTransaction("dependencies/anoma-pa-evm-1.1.0/test/examples/transactions/test_tx_reg_01_01.bin");
     }
 }
