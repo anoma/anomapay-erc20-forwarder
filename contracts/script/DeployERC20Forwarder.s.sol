f21da6d9880a2d8fd057322fbb8c802cf3ad9f7e
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-04-16 13:28:35 +0200

    build: use nightly pa-evm and risc0-deploytments release candidate

--- contracts/script/DeployERC20Forwarder.s.sol
+++ contracts/script/DeployERC20Forwarder.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import {IVersion} from "anoma-pa-evm-1.1.0/src/interfaces/IVersion.sol";
+import {IVersion} from "anoma-pa-evm-nightly/src/interfaces/IVersion.sol";
 
 import {Script} from "forge-std-1.15.0/src/Script.sol";
 
