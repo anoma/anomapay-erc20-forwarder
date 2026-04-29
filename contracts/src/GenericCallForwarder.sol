213b0d6ac71f29abea4575e2fa61dcdb1eb96db9
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-04-29 13:21:08 +0200

    WIP

---
+++ contracts/src/GenericCallForwarder.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {IERC1271} from "@openzeppelin-contracts-5.6.1/interfaces/IERC1271.sol";
+
+import {IProtocolAdapter} from "anoma-pa-evm-1.2.0-rc.0/src/interfaces/IProtocolAdapter.sol";
+import {ForwarderBase} from "./bases/ForwarderBase.sol";
+
+/// @title ERC20Forwarder
+/// @author Anoma Foundation, 2025
+/// @notice The ERC20 token forwarder contract allowing to swap ERC20 tokens on a DEX router.
+/// @custom:security-contact security@anoma.foundation
+contract GenericCallForwarder is IERC1271, ForwarderBase {
+    /// @notice The action struct to be consumed by the DAO's `execute` function resulting in an external call.
+    /// @param to The address to call.
+    /// @param value The native token value to be sent with the call.
+    /// @param data The bytes-encoded function selector and calldata for the call.
+    struct Call {
+        address to;
+        uint256 value;
+        bytes data;
+    }
+
+    /// @notice Emitted when calls are executed.
+    /// @param calls The array of calls executed.
+    /// @param execResults The array with the results of the executed calls.
+    event Executed(Call[] calls, bytes[] execResults);
+
+    /// @notice Emitted when a native token deposit has been made to the DAO.
+    /// @dev This event is intended to be emitted in the `receive` function and is therefore bound by the gas limitations for `send`/`transfer` calls introduced by [ERC-2929](https://eips.ethereum.org/EIPS/eip-2929).
+    /// @param sender The address of the sender.
+    /// @param amount The amount of native tokens deposited.
+    event NativeTokenDeposited(address sender, uint256 amount);
+
+    error CallFailed(uint256 index);
+
+    /// @notice Initializes the ERC-20 forwarder contract.
+    /// @param protocolAdapter The protocol adapter contract that can forward calls.
+    /// @param logicRef The reference to the logic function of the resource kind triggering the forward call.
+    /// been stopped.
+    constructor(address protocolAdapter, bytes32 logicRef) ForwarderBase(protocolAdapter, logicRef) {}
+
+    /// @inheritdoc IERC1271
+    function isValidSignature(bytes32 hash, bytes calldata signature) external pure override returns (bytes4) {
+        (hash, signature);
+
+        // NOTE: Authorization is happening on the resource triggering this call.
+
+        return IERC1271.isValidSignature.selector;
+    }
+
+    /// @notice Forwards a call wrapping or unwrapping ERC20 tokens based on the provided input.
+    /// @param input Contains the calls to make.
+    /// @return output The empty string signaling that the function call has succeeded.
+    // @dev Note: This method is reentrancy-protected by the `nonReentrant` modifier in `ForwarderBase.forwardCall`.
+    function _forwardCall(bytes calldata input) internal virtual override returns (bytes memory output) {
+        (Call[] memory calls) = abi.decode(input, (Call[]));
+
+        bool success;
+
+        uint256 nCalls = calls.length;
+        bytes[] memory execResults = new bytes[](nCalls);
+
+        for (uint256 i = 0; i < nCalls;) {
+            (success, execResults[i]) = calls[i].to.call{value: calls[i].value}(calls[i].data);
+
+            require(success, CallFailed({index: i}));
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        emit Executed({calls: calls, execResults: execResults});
+
+        output = "";
+    }
+
+    /// @notice Emits the `NativeTokenDeposited` event to track native token deposits that weren't made via the deposit
+    /// method.
+    /// @dev This call is bound by the gas limitations for `send`/`transfer` calls introduced by
+    /// [ERC-2929](https://eips.ethereum.org/EIPS/eip-2929). Gas cost increases in future hard forks might break this
+    /// function. As an alternative, [ERC-2930](https://eips.ethereum.org/EIPS/eip-2930)-type transactions using access
+    /// lists can be employed.
+    receive() external payable {
+        emit NativeTokenDeposited({sender: msg.sender, amount: msg.value});
+    }
+}
---
+++ contracts/test/GenericCallForwarder.t.sol
@@ -0,0 +1,188 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+import {Time} from "@openzeppelin-contracts-5.6.1/utils/types/Time.sol";
+import {IForwarder} from "anoma-pa-evm-1.2.0-rc.0/src/interfaces/IForwarder.sol";
+import {ProtocolAdapter} from "anoma-pa-evm-1.2.0-rc.0/src/ProtocolAdapter.sol";
+import {DeployRiscZeroContracts} from "anoma-risc0-deployments-1.0.0-rc.1/script/DeployRiscZeroContracts.s.sol";
+import {Test, Vm, console, stdError} from "forge-std-1.15.0/src/Test.sol";
+import {RiscZeroGroth16Verifier} from "risc0-risc0-ethereum-3.0.1/contracts/src/groth16/RiscZeroGroth16Verifier.sol";
+import {RiscZeroVerifierRouter} from "risc0-risc0-ethereum-3.0.1/contracts/src/RiscZeroVerifierRouter.sol";
+import {WETH} from "solady-0.1.26/src/tokens/WETH.sol";
+import {
+    IPermit2,
+    ISignatureTransfer
+} from "uniswap-permit2-0x000000000022D473030F116dDEE9F6B43aC78BA3/src/interfaces/IPermit2.sol";
+
+import {ERC20Forwarder} from "../src/ERC20Forwarder.sol";
+import {ERC20ForwarderPermit2} from "../src/ERC20ForwarderPermit2.sol";
+import {GenericCallForwarder} from "../src/GenericCallForwarder.sol";
+
+import {Permit2Signature} from "./libs/Permit2Signature.sol";
+import {DeployPermit2} from "./script/DeployPermit2.s.sol";
+
+contract GenericCallForwarderTest is Test {
+    using ERC20ForwarderPermit2 for ERC20ForwarderPermit2.Witness;
+    using Permit2Signature for Vm;
+
+    address internal constant _EMERGENCY_COMMITTEE = address(uint160(1));
+    uint128 internal constant _TRANSFER_AMOUNT = 1000;
+    bytes internal constant _EXPECTED_OUTPUT = "";
+    bytes32 internal constant _ACTION_TREE_ROOT = bytes32(uint256(0));
+
+    bytes32 internal _erc20ResourceLogicRef;
+    bytes32 internal _genericCallResourceLogicRef;
+
+    address internal _alice;
+    uint256 internal _alicePrivateKey;
+
+    ProtocolAdapter internal _pa;
+    IForwarder internal _erc20Fwd;
+    IForwarder internal _genericCallFwd;
+
+    IPermit2 internal _permit2;
+    WETH _weth;
+
+    ISignatureTransfer.PermitTransferFrom internal _defaultPermit;
+    bytes32 internal _defaultPermitSigR;
+    bytes32 internal _defaultPermitSigS;
+    uint8 internal _defaultPermitSigV;
+    bytes internal _defaultWrapInput;
+    bytes internal _defaultUnwrapInput;
+
+    function setUp() public virtual {
+        _erc20ResourceLogicRef = bytes32(uint256(1));
+        _genericCallResourceLogicRef = bytes32(uint256(2));
+
+        _alicePrivateKey = 0xc522337787f3037e9d0dcba4dc4c0e3d4eb7b1c65598d51c425574e8ce64d140;
+        _alice = vm.addr(_alicePrivateKey);
+
+        _weth = new WETH();
+
+        // Deploy the Permit2 contract
+        _permit2 = new DeployPermit2().run();
+
+        // Deploy RISC Zero contracts
+        (RiscZeroVerifierRouter router,, RiscZeroGroth16Verifier verifier) =
+            new DeployRiscZeroContracts().run({admin: msg.sender, guardian: msg.sender});
+
+        // Deploy the protocol adapter
+        _pa = new ProtocolAdapter(router, verifier.SELECTOR(), _EMERGENCY_COMMITTEE);
+
+        // Deploy the ERC20 forwarder
+        _erc20Fwd = new ERC20Forwarder({
+            protocolAdapter: address(_pa), emergencyCommittee: _EMERGENCY_COMMITTEE, logicRef: _erc20ResourceLogicRef
+        });
+        _genericCallFwd =
+            new GenericCallForwarder({protocolAdapter: address(_pa), logicRef: _genericCallResourceLogicRef});
+
+        _defaultPermit = ISignatureTransfer.PermitTransferFrom({
+            permitted: ISignatureTransfer.TokenPermissions({token: address(_weth), amount: _TRANSFER_AMOUNT}),
+            nonce: 123,
+            deadline: Time.timestamp() + 5 minutes
+        });
+
+        (_defaultPermitSigR, _defaultPermitSigS, _defaultPermitSigV) = vm.permitWitnessTransferFromSignature({
+            domainSeparator: _permit2.DOMAIN_SEPARATOR(),
+            permit: _defaultPermit,
+            privateKey: _alicePrivateKey,
+            spender: address(_erc20Fwd),
+            witness: ERC20ForwarderPermit2.Witness(_ACTION_TREE_ROOT).hash()
+        });
+
+        _defaultWrapInput = abi.encode(
+            /*       callType */
+            ERC20Forwarder.CallType.Wrap,
+            /*          token */
+            _defaultPermit.permitted.token,
+            /*         amount */
+            _defaultPermit.permitted.amount,
+            /*      wrap data */
+            ERC20Forwarder.WrapData({
+                nonce: _defaultPermit.nonce,
+                deadline: _defaultPermit.deadline,
+                owner: _alice,
+                actionTreeRoot: _ACTION_TREE_ROOT,
+                r: _defaultPermitSigR,
+                s: _defaultPermitSigS,
+                v: _defaultPermitSigV
+            })
+        );
+
+        _defaultUnwrapInput = abi.encode( /* callType */
+            ERC20Forwarder.CallType.Unwrap,
+            /*       token */
+            address(_weth),
+            /*      amount */
+            _TRANSFER_AMOUNT,
+            /* unwrap data */
+            ERC20Forwarder.UnwrapData({receiver: address(_genericCallFwd)})
+        );
+    }
+
+    /*function test_calls_allow_swapping_fund_on_a_dex() public {
+        _erc20.mint({to: _alice, value: _TRANSFER_AMOUNT});
+        uint256 startBalanceAlice = _erc20.balanceOf(_alice);
+        uint256 startBalanceForwarder = _erc20.balanceOf(address(_erc20Fwd));
+
+        vm.prank(_alice);
+        _erc20.approve(address(_permit2), type(uint256).max);
+
+        vm.prank(address(_pa));
+        bytes memory output = _erc20Fwd.forwardCall({logicRef: _logicRef, input: _defaultWrapInput});
+
+        assertEq(keccak256(output), keccak256(_EXPECTED_OUTPUT));
+        assertEq(_erc20.balanceOf(_alice), startBalanceAlice - _TRANSFER_AMOUNT);
+        assertEq(_erc20.balanceOf(address(_erc20Fwd)), startBalanceForwarder + _TRANSFER_AMOUNT);
+    }*/
+
+    function test_calls_allow_to_unwrap_native_tokens() public {
+        // Fund ERC20 Forwarder with WETH
+        {
+            vm.deal(address(_erc20Fwd), _TRANSFER_AMOUNT);
+            vm.prank(address(_erc20Fwd));
+            _weth.deposit{value: _TRANSFER_AMOUNT}();
+        }
+
+        assertEq(_weth.balanceOf(address(_erc20Fwd)), _TRANSFER_AMOUNT);
+        assertEq(_weth.balanceOf(address(_genericCallFwd)), 0);
+        assertEq(_alice.balance, 0);
+
+        // Mock ERC20Forwarder call (triggered by TokenTransfer resource)
+        {
+            // Unwrap WETH-R into the generic call forwarder
+            vm.prank(address(_pa));
+            bytes memory output1 = _erc20Fwd.forwardCall({logicRef: _erc20ResourceLogicRef, input: _defaultUnwrapInput});
+            assertEq(keccak256(output1), keccak256(_EXPECTED_OUTPUT));
+        }
+
+        assertEq(_weth.balanceOf(address(_erc20Fwd)), 0);
+        assertEq(_weth.balanceOf(address(_genericCallFwd)), _TRANSFER_AMOUNT);
+        assertEq(_alice.balance, 0);
+
+        // Mock GenericCallForwarder call (triggered by GenericCall resource)
+        {
+            GenericCallForwarder.Call[] memory genericCalls = new GenericCallForwarder.Call[](2);
+            // Call 1: Unwrap WETH
+            genericCalls[0] = GenericCallForwarder.Call({
+                to: address(_weth), value: 0, data: abi.encodeCall(WETH.withdraw, uint256(_TRANSFER_AMOUNT))
+            });
+            // Call 2: Transfer ETH
+            genericCalls[1] = GenericCallForwarder.Call({to: _alice, value: _TRANSFER_AMOUNT, data: ""});
+            bytes memory _unwrapWethAndTransferEthInput = abi.encode(genericCalls);
+
+            vm.prank(address(_pa));
+            vm.expectEmit(address(_genericCallFwd));
+            emit GenericCallForwarder.NativeTokenDeposited({sender: address(_weth), amount: _TRANSFER_AMOUNT});
+
+            bytes memory output2 = _genericCallFwd.forwardCall({
+                logicRef: _genericCallResourceLogicRef, input: _unwrapWethAndTransferEthInput
+            });
+            assertEq(keccak256(output2), keccak256(_EXPECTED_OUTPUT));
+        }
+
+        assertEq(_weth.balanceOf(address(_erc20Fwd)), 0);
+        assertEq(_weth.balanceOf(address(_genericCallFwd)), 0);
+        assertEq(_alice.balance, _TRANSFER_AMOUNT);
+    }
+}
