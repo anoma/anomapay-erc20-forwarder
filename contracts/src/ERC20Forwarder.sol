7fce6e3458a77f4e4b6d929c6179feadaf3e9d4a
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-06-24 15:19:35 +0200

    chore(contracts, bindings): bump versions

--- contracts/src/ERC20Forwarder.sol
+++ contracts/src/ERC20Forwarder.sol
@@ -94,7 +94,7 @@ contract ERC20Forwarder is IVersion, EmergencyMigratableForwarderBase {
 
     /// @inheritdoc IVersion
     function getVersion() external pure override returns (bytes32 version) {
-        version = "1.1.0-rc.2";
+        version = "1.1.0-rc.3";
     }
 
     // slither-disable-start dead-code /* NOTE: This code is not dead and falsely flagged as such by slither. */
